5995b67d0683dfc6acaa7f8b803f48480374e41f
AliciaSabat <40358691+AliciaSabat@users.noreply.github.com>
2018-11-28 12:08:15 +0000

    perameters attempt 1

--- application.py
+++ application.py
@@ -1,5 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello Alicia!"
+import os.path
+import os
+import flask
+
+app = flask.Flask(__name__)
+@app.route('/name')
+
+
+def name():
+    hiname = flask.request.args.get('name')
+    #instance = load()
+    return flask.jsonify("Hello " , hiname,  "!" )
+
